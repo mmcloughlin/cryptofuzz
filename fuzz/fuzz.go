a444a3e97654a3c06dbda78e707c1a60052d79ad
Michael McLoughlin <mmcloughlin@gmail.com>
2019-01-31 20:02:31 -0800

    fuzz: generic hash fuzzer

---
+++ fuzz/fuzz.go
@@ -0,0 +1,30 @@
+// Package fuzz provides generic fuzzers for crypto interfaces.
+package fuzz
+
+import (
+	"bytes"
+	"hash"
+	"io"
+)
+
+// Hash compares two hash implementations on the given fuzz data.
+func Hash(data []byte, a, b hash.Hash) int {
+	h := io.MultiWriter(a, b)
+	n, err := h.Write(data)
+	if err != nil {
+		panic(err.Error())
+	}
+	if n != len(data) {
+		panic("short write")
+	}
+
+	// Compare checksums.
+	asum := a.Sum(nil)
+	bsum := a.Sum(nil)
+
+	if !bytes.Equal(asum, bsum) {
+		panic("mismatch")
+	}
+
+	return 0
+}
