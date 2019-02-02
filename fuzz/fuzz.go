5f8f44c9715de54e4e498d603bc7760132c010c8
Michael McLoughlin <mmcloughlin@gmail.com>
2019-02-01 19:32:55 -0800

    fuzz: write to hash in random chunks

--- fuzz/fuzz.go
+++ fuzz/fuzz.go
@@ -3,19 +3,36 @@ package fuzz
 
 import (
 	"bytes"
+	"encoding/binary"
 	"hash"
 	"io"
+	"math/rand"
 )
 
 // Hash compares two hash implementations on the given fuzz data.
 func Hash(data []byte, a, b hash.Hash) int {
-	h := io.MultiWriter(a, b)
-	n, err := h.Write(data)
-	if err != nil {
-		panic(err.Error())
+	// Seed an RNG.
+	if len(data) < 4 {
+		return -1
 	}
-	if n != len(data) {
-		panic("short write")
+	seed := binary.BigEndian.Uint32(data)
+	data = data[4:]
+	r := rand.New(rand.NewSource(int64(seed)))
+
+	// Write to hash in random chunks.
+	h := io.MultiWriter(a, b)
+	for len(data) > 0 {
+		m := 1 + r.Intn(len(data))
+		chunk := data[:m]
+		data = data[m:]
+
+		n, err := h.Write(chunk)
+		if err != nil {
+			panic(err.Error())
+		}
+		if n != len(chunk) {
+			panic("short write")
+		}
 	}
 
 	// Compare checksums.
