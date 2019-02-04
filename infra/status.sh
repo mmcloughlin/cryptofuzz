990985238a77c112d99530b6b3edbe7072c6ccde
Michael McLoughlin <mmcloughlin@gmail.com>
2019-02-04 00:45:25 -0800

    infra: remove crappy status.sh script

--- infra/status.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -ex
-
-ips="34.214.180.228 35.167.29.72 18.236.144.186 34.216.21.94 52.88.61.154"
-
-for ip in $ips; do
-    ssh ubuntu@$ip tail -n 2 /opt/cryptofuzz/target/*/stderr
-done
