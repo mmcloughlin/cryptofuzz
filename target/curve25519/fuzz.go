12578f58e92b19af6435103a3463b293d0d4d017
Michael McLoughlin <mmcloughlin@gmail.com>
2019-02-03 17:32:12 -0800

    curve25519: fix potential bug
    We were reusing the `in` and `base` inputs, but these may be modified by
    the ScalarMult function. This diff initializes these variables before
    calling the second ScalarMult.
    
    Updates #1

--- target/curve25519/fuzz.go
+++ target/curve25519/fuzz.go
@@ -2,6 +2,7 @@ package curve25519
 
 import (
 	"bytes"
+	"fmt"
 
 	asm "github.com/mmcloughlin/cryptofuzz/target/curve25519/fork/asm"
 	noasm "github.com/mmcloughlin/cryptofuzz/target/curve25519/fork/noasm"
@@ -12,15 +13,22 @@ func Fuzz(data []byte) int {
 		return -1
 	}
 
+	var a, b [32]byte
 	var in, base [32]byte
+
 	copy(in[:], data[:32])
 	copy(base[:], data[32:64])
-
-	var a, b [32]byte
 	asm.ScalarMult(&a, &in, &base)
+
+	copy(in[:], data[:32])
+	copy(base[:], data[32:64])
 	noasm.ScalarMult(&b, &in, &base)
 
 	if !bytes.Equal(a[:], b[:]) {
+		fmt.Printf("  in = %x\n", in)
+		fmt.Printf("base = %x\n", base)
+		fmt.Printf("   a = %x\n", a)
+		fmt.Printf("   b = %x\n", b)
 		panic("mismatch")
 	}
 
