850be13c95f8132b2da1ccfa431052dc5176b115
Michael McLoughlin <mmcloughlin@gmail.com>
2019-02-12 21:47:04 -0800

    fuzz: factor out some helper functions

--- fuzz/fuzz.go
+++ fuzz/fuzz.go
@@ -5,36 +5,57 @@ import (
 	"bytes"
 	"crypto/elliptic"
 	"encoding/binary"
+	"errors"
 	"hash"
 	"io"
 	"math/rand"
 )
 
-// Hash compares two hash implementations on the given fuzz data.
-func Hash(data []byte, a, b hash.Hash) int {
-	// Seed an RNG.
+// ErrShort is returned when fuzz data is too short.
+var ErrShort = errors.New("fuzz: provided data too short")
+
+// Rand builds a random source from fuzz data.
+func Rand(data []byte) (*rand.Rand, []byte, error) {
 	if len(data) < 4 {
-		return -1
+		return nil, nil, ErrShort
 	}
 	seed := binary.BigEndian.Uint32(data)
 	data = data[4:]
 	r := rand.New(rand.NewSource(int64(seed)))
+	return r, data, nil
+}
+
+// MustWrite writes b to w and panics if anything goes wrong.
+func MustWrite(w io.Writer, b []byte) {
+	n, err := w.Write(b)
+	if err != nil {
+		panic(err.Error())
+	}
+	if n != len(b) {
+		panic("short write")
+	}
+}
+
+// RandWrite writes b to w in random-sized chunks, based on the given random source.
+func RandWrite(w io.Writer, r *rand.Rand, b []byte) {
+	for len(b) > 0 {
+		m := 1 + r.Intn(len(b))
+		chunk := b[:m]
+		b = b[m:]
+		MustWrite(w, chunk)
+	}
+}
+
+// Hash compares two hash implementations on the given fuzz data.
+func Hash(data []byte, a, b hash.Hash) int {
+	r, data, err := Rand(data)
+	if err != nil {
+		return -1
+	}
 
 	// Write to hash in random chunks.
 	h := io.MultiWriter(a, b)
-	for len(data) > 0 {
-		m := 1 + r.Intn(len(data))
-		chunk := data[:m]
-		data = data[m:]
-
-		n, err := h.Write(chunk)
-		if err != nil {
-			panic(err.Error())
-		}
-		if n != len(chunk) {
-			panic("short write")
-		}
-	}
+	RandWrite(h, r, data)
 
 	// Compare checksums.
 	asum := a.Sum(nil)
