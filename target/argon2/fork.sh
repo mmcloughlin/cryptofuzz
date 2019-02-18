f4d8b86369af180dbfda6a326cdb3d14db00da5d
Michael McLoughlin <mmcloughlin@gmail.com>
2019-02-17 19:15:43 -0800

    target/argon2: fork (#7)

---
+++ target/argon2/fork.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+
+source ../../script/lib.sh
+
+pkg="golang.org/x/crypto/argon2"
+this_pkg=$(target_import_path argon2)
+dst="fork"
+
+# Clean existing forked packages.
+rm -rf ${dst}
+
+# Fork two versions.
+heading 'fork asm version'
+fork_package_for_arch ${pkg} amd64 ${dst}/asm
+
+heading 'fork noasm version (mips architecture)'
+fork_package_for_arch ${pkg} mips ${dst}/noasm
+
+# Confirm they build.
+heading "go build"
+go build -v ./${dst}/...
+
+heading "go test"
+go test -v -cover ./${dst}/*asm
+
+# Report.
+heading "forked packages"
+tree ${dst}
---
+++ target/argon2/fork/noasm/blamka_ref.go
@@ -0,0 +1,14 @@
+// Copyright 2017 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+
+package argon2
+
+func processBlock(out, in1, in2 *block) {
+	processBlockGeneric(out, in1, in2, false)
+}
+
+func processBlockXOR(out, in1, in2 *block) {
+	processBlockGeneric(out, in1, in2, true)
+}
